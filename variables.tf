8fe938725ffa895e9b7c8c09104726e0b0c19ee1
bhanupashnik <89307143+bhanupashnik@users.noreply.github.com>
2022-04-28 13:54:22 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -60,7 +60,7 @@ variable "fixed_pvt_ip" {
 # Instances
 variable "ansible_instance" {
   description = "The Ansible Server Instance."
-  default     = { instance_type : "t2.micro", pvt_ip : "10.0.1.13" }
+  default     = { instance_type : "t2.micro", pvt_ip : "10.0.4.10" }
 }
 
 variable "zookeeper_instances" {
